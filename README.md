08eb1d495b942be39c89c8a988587adb5b5606c7
navspeak <navneet.sahay@gmail.com>
2020-03-17 10:33:26 -0400

    wsdl

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 * mvn archetype:generate -Dfilter=org.apache.cxf.archetype:
+* Document/Literal sends full XML document as message whereas rpc makes calls to remote objects and provides features for handling binding and encoding
 
---
+++ src/main/java/com/nav/cxfdemo/com/nav/cxfdemo/tools/JavaToWSCodeGenerator.java
@@ -0,0 +1,17 @@
+package com.nav.cxfdemo.com.nav.cxfdemo.tools;
+
+import org.apache.cxf.tools.java2ws.JavaToWS;
+
+//CXF java2wsdl Example
+public class JavaToWSCodeGenerator {
+    public static void main(String[] args) {
+        try {
+            JavaToWS.main(new String[]{"-d", "src", "-o", "main/resources/wsdl/HelloWorldImpl.wsdl", "-createxsdimports", "-wsdl",
+                    "com.nav.cxfdemo.HelloWorldImpl"});
+            System.out.println("finished %%%%%%%%%%");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
